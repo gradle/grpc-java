470b763c06293f2f0aecad9c19441f48d8a15ed5
Tom Tresansky <ttresansky@gradle.com>
2023-08-02 14:13:06 -0400

    Add performance experiments and results

---
+++ comparisons/bazel-and-all-gradle-no-cc.js
@@ -0,0 +1,7 @@
+// Comparison data from /Users/ttresansky/Projects/grpc-java/comparisons/bazel-and-all-gradle.csv
+
+var data = [
+    ['Scenario', 'Bazel', 'Gradle 8.2.1', 'Gradle 8.3-rc-2'],
+    ['ABI Change', 6859, 19218, 17997],
+    ['Non-ABI Change', 1822, 18762, 18327]
+];
--- run.performance.experiments.sh
+++ run.performance.experiments.sh
@@ -13,11 +13,9 @@ gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performa
 gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios abiChange --bazel --output-dir results/abiChange/bazel
 gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios nonAbiChange --gradle-version 8.2.1 --output-dir results/nonAbiChange/gradle-8.2.1
 gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios abiChange --gradle-version 8.2.1 --output-dir results/abiChange/gradle-8.2.1
-gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios nonAbiChange --gradle-version 8.3-rc-2 --output-dir results/nonAbiChange/gradle-8.3-RC-1
-gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios abiChange --gradle-version 8.3-rc-2 --output-dir results/abiChange/gradle-8.3-RC-1
+gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios nonAbiChange --gradle-version 8.3-rc-2 --output-dir results/nonAbiChange/gradle-8.3-RC-2
+gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios abiChange --gradle-version 8.3-rc-2 --output-dir results/abiChange/gradle-8.3-RC-2
 
-# Process results into comparisons
-python3 fix-bench-csvs.py comparisons/bazel-and-gradle-8.2.1.csv results/abiChange/bazel/benchmark.csv results/abiChange/gradle-8.2.1/benchmark.csv results/nonAbiChange/bazel/benchmark.csv results/nonAbiChange/gradle-8.2.1/benchmark.csv
-python3 fix-bench-csvs.py comparisons/bazel-and-gradle-8.3-RC-1.csv results/abiChange/bazel/benchmark.csv results/abiChange/gradle-8.3-RC-1/benchmark.csv results/nonAbiChange/bazel/benchmark.csv results/nonAbiChange/gradle-8.3-RC-1/benchmark.csv
-python3 fix-bench-csvs.py comparisons/gradle-8.2.1-and-8.3-RC-1.csv results/abiChange/gradle-8.2.1-with-cc/benchmark.csv results/nonAbiChange/gradle-8.2.1/benchmark.csv results/abiChange/gradle-8.3-RC-1/benchmark.csv results/nonAbiChange/gradle-8.3-RC-1/benchmark.csv
-python3 fix-bench-csvs.py comparisons/bazel-and-gradle-8.2.1-and-8.3-RC-1.csv results/abiChange/bazel/benchmark.csv results/nonAbiChange/bazel/benchmark.csv results/abiChange/gradle-8.2.1/benchmark.csv results/nonAbiChange/gradle-8.2.1/benchmark.csv results/abiChange/gradle-8.3-RC-1/benchmark.csv results/nonAbiChange/gradle-8.3-RC-1/benchmark.csv
+# Process results into comparisons (requires scripts in other experiment repo)
+#python3 ~/Projects/compilation-avoidance-experiment/fix-bench-csvs.py comparisons/bazel-and-all-gradle.csv results/abiChange/bazel/benchmark.csv results/nonAbiChange/bazel/benchmark.csv results/abiChange/gradle-8.2.1/benchmark.csv results/nonAbiChange/gradle-8.2.1/benchmark.csv results/abiChange/gradle-8.3-RC-2/benchmark.csv results/nonAbiChange/gradle-8.3-RC-2/benchmark.csv
+#~/Projects/compilation-avoidance-experiment/chart-averages.groovy ~/Projects/grpc-java/comparisons/bazel-and-all-gradle.csv ~/Projects/grpc-java/comparisons/bazel-and-all-gradle-no-cc.js
