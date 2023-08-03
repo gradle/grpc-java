19d66b72e30530163a17eff18ac84b33a792d7e2
Tom Tresansky <ttresansky@gradle.com>
2023-08-03 07:43:50 -0400

    Update comparisons script

--- run.performance.experiments.sh
+++ run.performance.experiments.sh
@@ -13,9 +13,9 @@ gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performa
 gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios abiChange --bazel --output-dir results/abiChange/bazel
 gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios nonAbiChange --gradle-version 8.2.1 --output-dir results/nonAbiChange/gradle-8.2.1
 gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios abiChange --gradle-version 8.2.1 --output-dir results/abiChange/gradle-8.2.1
-gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios nonAbiChange --gradle-version 8.3-rc-2 --output-dir results/nonAbiChange/gradle-8.3-RC-2
-gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios abiChange --gradle-version 8.3-rc-2 --output-dir results/abiChange/gradle-8.3-RC-2
+gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios nonAbiChange --gradle-version 8.3-rc-3 --output-dir results/nonAbiChange/gradle-8.3-rc-3
+gradle-profiler --benchmark --warmups 5 --iterations 10 --scenario-file performance.scenarios abiChange --gradle-version 8.3-rc-3 --output-dir results/abiChange/gradle-8.3-rc-3
 
 # Process results into comparisons (requires scripts in other experiment repo)
-#python3 ~/Projects/compilation-avoidance-experiment/fix-bench-csvs.py comparisons/bazel-and-all-gradle.csv results/abiChange/bazel/benchmark.csv results/nonAbiChange/bazel/benchmark.csv results/abiChange/gradle-8.2.1/benchmark.csv results/nonAbiChange/gradle-8.2.1/benchmark.csv results/abiChange/gradle-8.3-RC-2/benchmark.csv results/nonAbiChange/gradle-8.3-RC-2/benchmark.csv
+#python3 ~/Projects/compilation-avoidance-experiment/fix-bench-csvs.py comparisons/bazel-and-all-gradle.csv results/abiChange/bazel/benchmark.csv results/nonAbiChange/bazel/benchmark.csv results/abiChange/gradle-8.2.1/benchmark.csv results/nonAbiChange/gradle-8.2.1/benchmark.csv results/abiChange/gradle-8.3-rc-3/benchmark.csv results/nonAbiChange/gradle-8.3-rc-3/benchmark.csv
 #~/Projects/compilation-avoidance-experiment/chart-averages.groovy ~/Projects/grpc-java/comparisons/bazel-and-all-gradle.csv ~/Projects/grpc-java/comparisons/bazel-and-all-gradle-no-cc.js
