b377d60130ac027f476e8f11d0ee8041a5e55ec2
Tom Tresansky <ttresansky@gradle.com>
2023-08-03 10:16:03 -0400

    New data from experiment runs after project modifications

--- comparisons/bazel-and-all-gradle-no-cc.js
+++ comparisons/bazel-and-all-gradle-no-cc.js
@@ -1,7 +1,7 @@
 // Comparison data from /Users/ttresansky/Projects/grpc-java/comparisons/bazel-and-all-gradle.csv
 
 var data = [
-    ['Scenario', 'Bazel', 'Gradle 8.2.1', 'Gradle 8.3-rc-2'],
-    ['ABI Change', 6859, 19218, 17997],
-    ['Non-ABI Change', 1822, 18762, 18327]
+    ['Scenario', 'Bazel', 'Gradle 8.2.1', 'Gradle 8.3-rc-3'],
+    ['ABI Change', 11579, 17676, 14520],
+    ['Non-ABI Change', 2196, 7877, 5580]
 ];
